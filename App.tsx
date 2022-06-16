86d46f4f258b491be4d29174a4278118d87a17e2
gustavo.kuze <gustavo.kuze@lighthouseit.com.br>
2022-06-16 15:02:23 -0300

    feat: basic spotify user login

--- App.tsx
+++ App.tsx
@@ -1,9 +1,68 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import axios from "axios";
+import { useEffect, useState } from 'react';
+import { ResponseType, useAuthRequest } from "expo-auth-session";
 
 export default function App() {
+
+  const [token, setToken] = useState("");
+  const [request, response, promptAsync] = useAuthRequest(
+    {
+      responseType: ResponseType.Token,
+      clientId: "1ca9457fdc4843d0ade2358ab67465a8",
+      scopes: [
+        "user-read-currently-playing",
+        "user-read-recently-played",
+        "user-read-playback-state",
+        "user-top-read",
+        "user-modify-playback-state",
+        "streaming",
+        "user-read-email",
+        "user-read-private",
+      ],
+      usePKCE: false,
+      redirectUri: "exp://127.0.0.1:19000/",
+    },
+    {
+      authorizationEndpoint: "https://accounts.spotify.com/authorize",
+      tokenEndpoint: "https://accounts.spotify.com/api/token",
+    }
+  );
+
+  useEffect(() => {
+    if (response?.type === "success") {
+      const { access_token } = response.params;
+      setToken(access_token);
+    }
+  }, [response]);
+
+  useEffect(() => {
+    if (token) {
+      axios("https://api.spotify.com/v1/me/top/tracks?time_range=short_term", {
+        method: "GET",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+          Authorization: "Bearer " + token,
+        },
+      })
+        .then((response) => {
+          console.log("response", response);
+        })
+        .catch((error) => {
+          console.log("error", error.message);
+        });
+    }
+  });
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Button
+        title="Login with Spotify"
+        onPress={() => {
+          promptAsync();
+        }}
+      />
     </View>
   );
 }
