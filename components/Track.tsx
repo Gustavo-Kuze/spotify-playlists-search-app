f01751adf134225aafde7c0dfb418f2c887ee1cc
Gustavo-Kuze <gustavoksilva@gmail.com>
2022-06-17 18:42:23 -0300

    feat: play icon toggle

--- components/Track.tsx
+++ components/Track.tsx
@@ -1,12 +1,14 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 import {
   Text,
   Image,
   HStack,
   VStack,
   Pressable,
+  Icon,
 } from "native-base";
 import { SpotifyTrack } from '../types';
+import { Ionicons } from '@expo/vector-icons';
 
 interface Props {
   track: SpotifyTrack;
@@ -17,6 +19,11 @@ const Track: FC<Props> = ({
   track,
   onPress,
 }) => {
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  const togglePlay = () => {
+    setIsPlaying(!isPlaying);
+  }
 
   return (
     <Pressable
@@ -32,32 +39,47 @@ const Track: FC<Props> = ({
         onPress(track);
       }}
     >
-      <HStack space={3}>
-        <Image
-          size="md"
-          source={{
-            uri: (track.album.images[0] || {}).url
-          }} />
-        <VStack >
-          <Text
-            _dark={{
-              color: "warmGray.50"
-            }}
-            color="coolGray.800"
-            bold
-            maxW="80"
-            textBreakStrategy="balanced"
-          >
-            {track.name}
-          </Text>
-          <Text
-            color="coolGray.600"
-            _dark={{
-              color: "warmGray.200"
-            }}>
-            Artista: {track.artists[0].name}
-          </Text>
-        </VStack>
+      <HStack justifyContent="space-between" alignItems="center">
+        <HStack space={3} maxW="2/3">
+          <Image
+            size="md"
+            source={{
+              uri: (track.album.images[0] || {}).url
+            }} />
+          <VStack >
+            <Text
+              _dark={{
+                color: "warmGray.50"
+              }}
+              color="coolGray.800"
+              bold
+              maxW="80"
+              textBreakStrategy="balanced"
+            >
+              {track.name}
+            </Text>
+            <Text
+              color="coolGray.600"
+              _dark={{
+                color: "warmGray.200"
+              }}>
+              Artista: {track.artists[0].name}
+            </Text>
+          </VStack>
+        </HStack>
+        <Pressable
+          onPress={() => {
+            togglePlay();
+          }}
+        >
+          <Icon
+            as={Ionicons}
+            name={isPlaying ? "pause-circle-outline" : "play-circle-outline"}
+            size="3xl"
+            color="primary.500"
+            mt="2"
+          />
+        </Pressable>
       </HStack>
     </Pressable>
   );
