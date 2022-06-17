63e3df0ccd91f3202d61ec8d8e912f44ccaf0d60
Gustavo-Kuze <gustavoksilva@gmail.com>
2022-06-17 08:50:24 -0300

    refactor: moved playlist item to separate component

---
+++ components/Playlist.tsx
@@ -0,0 +1,70 @@
+import React, { FC } from 'react';
+import {
+  Box,
+  Text,
+  Image,
+  HStack,
+  VStack,
+} from "native-base";
+import { SpotifyPlaylist } from '../types';
+
+interface Props {
+  playlist: SpotifyPlaylist
+}
+
+const Playlist: FC<Props> = ({
+  playlist,
+}) => {
+
+  return (
+    <Box borderBottomWidth="1" _dark={{
+      borderColor: "gray.600"
+    }}
+      borderColor="coolGray.200"
+      pl="4"
+      pr="5"
+      py="2"
+    >
+      <HStack space={3}>
+        <Image
+          size="md"
+          source={{
+            uri: playlist.images[0].url
+          }} />
+        <VStack >
+          <Text
+            _dark={{
+              color: "warmGray.50"
+            }}
+            color="coolGray.800"
+            bold
+            maxW="80"
+            textBreakStrategy="balanced"
+          >
+            {playlist.name}
+          </Text>
+          <Text
+            color="coolGray.600"
+            _dark={{
+              color: "warmGray.200"
+            }}>
+            Criada por {playlist.owner.display_name}
+          </Text>
+          <Text
+            mt="4"
+            fontSize="sm"
+            _dark={{
+              color: "warmGray.50"
+            }}
+            color="coolGray.800"
+            alignSelf="flex-start"
+          >
+            Total de músicas: {playlist.tracks.total}
+          </Text>
+        </VStack>
+      </HStack>
+    </Box>
+  );
+};
+
+export default Playlist;
--- screens/Playlists.tsx
+++ screens/Playlists.tsx
@@ -3,15 +3,15 @@ import {
   Box,
   Heading,
   FlatList,
-  Text,
-  Image,
   HStack,
-  VStack,
   Spinner,
   Center,
+  IconButton,
 } from "native-base";
 import { useSelector } from "react-redux";
 import { PlaylistsReducer } from "../redux/repos/playlists";
+import Playlist from "../components/Playlist";
+import { Ionicons } from '@expo/vector-icons';
 
 const Playlists: FC = () => {
   const { isLoading, items } = useSelector((state: { playlists: PlaylistsReducer }) => state.playlists);
@@ -24,9 +24,23 @@ const Playlists: FC = () => {
       _light={{ bg: 'blueGray.50' }}
     >
       <Box>
-        <Heading fontSize="xl" p="4" pb="3">
-          Playlists
-        </Heading>
+        <HStack justifyContent={'space-between'}>
+          <Heading fontSize="xl" p="4" pb="3">
+            Playlists
+          </Heading>
+
+          <IconButton
+            size={'lg'}
+            variant="link"
+            _icon={{
+              as: Ionicons,
+              name: "filter"
+            }}
+            onPress={() => {
+              console.log('filter');
+            }}
+          />
+        </HStack>
         {
           isLoading ? (
             <HStack space={2} justifyContent="center">
@@ -39,57 +53,7 @@ const Playlists: FC = () => {
             <FlatList
               data={items}
               pb="12"
-              renderItem={({
-                item
-              }) => (
-                <Box borderBottomWidth="1" _dark={{
-                  borderColor: "gray.600"
-                }}
-                  borderColor="coolGray.200"
-                  pl="4"
-                  pr="5"
-                  py="2"
-                >
-                  <HStack space={3}>
-                    <Image
-                      size="md"
-                      source={{
-                        uri: item.images[0].url
-                      }} />
-                    <VStack >
-                      <Text
-                        _dark={{
-                          color: "warmGray.50"
-                        }}
-                        color="coolGray.800"
-                        bold
-                        maxW="80"
-                        textBreakStrategy="balanced"
-                      >
-                        {item.name}
-                      </Text>
-                      <Text
-                        color="coolGray.600"
-                        _dark={{
-                          color: "warmGray.200"
-                        }}>
-                        Criada por {item.owner.display_name}
-                      </Text>
-                      <Text
-                        mt="4"
-                        fontSize="sm"
-                        _dark={{
-                          color: "warmGray.50"
-                        }}
-                        color="coolGray.800"
-                        alignSelf="flex-start"
-                      >
-                        Total de músicas: {item.tracks.total}
-                      </Text>
-                    </VStack>
-                  </HStack>
-                </Box>
-              )}
+              renderItem={({ item }) => <Playlist playlist={item} />}
               keyExtractor={item => item.id} />
           )
         }
